0a2de8a154cf9e2421b9f1d30b4d413d8fbb640b
Rafael Veiga <rafaelveig@hotmail.com>
2024-08-09 17:10:57 -0300

    🔧 chore(package.json): add @types/passport-jwt dependency 🚀 feat(app.module.ts): update AppModule imports and providers for new modules and interceptors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,62 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {
+  ClassSerializerInterceptor,
+  Module,
+  ValidationPipe,
+} from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { LoggerErrorInterceptor, LoggerModule } from 'nestjs-pino'
+import pinoConfig from './infra/config/pino'
+import { EnvModule } from './infra/config/env/env.module'
+import { DatabaseModule } from './infra/database/database.module'
 
+import { UsersModule } from './domain/users/users.module'
+import { AuthModule } from './domain/auth/auth.module'
+import { HttpExceptionFilter } from './infra/http/filters/http-exception'
+import { APP_PIPE, APP_INTERCEPTOR, Reflector, APP_FILTER } from '@nestjs/core'
+import { ValidationRequestException } from './core/errors/validation-request-exception'
+import { TutorialsModule } from './domain/tutorials/tutorials.module'
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    LoggerModule.forRoot(pinoConfig),
+    EnvModule,
+    DatabaseModule,
+    UsersModule,
+    AuthModule,
+    TutorialsModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useFactory: () => {
+        return new ValidationPipe({
+          transform: true,
+          validateCustomDecorators: true,
+          exceptionFactory(errors) {
+            return new ValidationRequestException(errors)
+          },
+        })
+      },
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      inject: [Reflector],
+      useFactory: (reflector: Reflector) => {
+        return new ClassSerializerInterceptor(reflector, {
+          excludeExtraneousValues: true,
+          enableImplicitConversion: true,
+        })
+      },
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerErrorInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
